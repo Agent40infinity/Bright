a3c75d8389038181e810ee3a75b4497938fabd9c
Aiden Nathan <agent9@y7mail.com>
2021-08-13 22:00:19 +1000

    Keybinds implementations
    - GameManager Creation
    - Keybinds implementation
    - Player Creation begun
    - Player keybinds completely hooked up

--- Bright/Assets/Scripts/Player/Player.cs
+++ Bright/Assets/Scripts/Player/Player.cs
@@ -4,15 +4,144 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Vector2 input;
+    public float speed;
+    public Rigidbody2D rigid;
+
+    public InteractState interactState = InteractState.Idle;
+
+    public WispMode wispMode = WispMode.Idle;
+    public WispMode previousMode;
+
+    public void Update()
     {
-        
+        Keypress();
+        Interaction();
+
+        switch (interactState)
+        {
+            case InteractState.Idle:
+                input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+                break;
+            case InteractState.Occupied:
+                input = Vector2.zero;
+                break;
+        }
+
+        switch (wispMode)
+        {
+            case WispMode.Blade:
+                Blade();
+                break;
+            case WispMode.Projectile:
+                Projectile();
+                break;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Keypress()
     {
-        
+        if (Input.GetKey(GameManager.keybind["Heal"]))
+        {
+            if (wispMode != WispMode.Healing)
+            {
+                previousMode = wispMode;
+            }
+            wispMode = WispMode.Healing;
+            interactState = InteractState.Occupied;
+
+            Healing();
+        }
+        else if (Input.GetKeyUp(GameManager.keybind["Heal"]))
+        {
+            wispMode = previousMode;
+            interactState = InteractState.Idle;
+
+            ResetHealing();
+        }
+
+        if (Input.GetKeyDown(GameManager.keybind["SwitchWeapon"]))
+        {
+            if (wispMode == WispMode.Blade)
+            {
+                wispMode = WispMode.Projectile;
+            }
+            else
+            {
+                wispMode = WispMode.Blade;
+            }
+        }
     }
+
+    public void Interaction()
+    {
+        if (Input.GetKeyDown(GameManager.keybind["Interact"]))
+        { 
+        
+            }
+    }
+
+    public void FixedUpdate()
+    {
+        Movement();
+    }
+
+    public void Movement()
+    {
+        rigid.velocity = input * speed;
+    }
+
+
+
+    public void Blade()
+    {
+        if (Input.GetKeyDown(GameManager.keybind["Melee"]))
+        {
+
+        }
+    }
+
+    public void Projectile()
+    {
+        if (Input.GetKey(GameManager.keybind["ShootUp"]))
+        {
+
+        }
+        else if (Input.GetKey(GameManager.keybind["ShootDown"]))
+        { 
+        
+        }
+        else if (Input.GetKey(GameManager.keybind["ShootLeft"]))
+        {
+
+        }
+        else if (Input.GetKey(GameManager.keybind["ShootRight"]))
+        {
+
+        }
+    }
+
+    public void Healing()
+    {
+
+    }
+
+    public void ResetHealing()
+    { 
+    
+    }
+}
+
+public enum InteractState
+{ 
+    Idle,
+    Occupied
+}
+
+public enum WispMode
+{ 
+    Blade,
+    Projectile,
+    Healing,
+    Idle
 }
---
+++ Bright/Assets/Scripts/Utility/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static Dictionary<string, KeyCode> keybind = new Dictionary<string, KeyCode> //Dictionary to store the keybinds.
+    {
+        { "MoveUp", KeyCode.W },
+        { "MoveDown", KeyCode.S },
+        { "MoveLeft", KeyCode.A },
+        { "MoveRight", KeyCode.D },
+        { "ShootUp", KeyCode.UpArrow },
+        { "ShootDown", KeyCode.DownArrow },
+        { "ShootLeft", KeyCode.LeftArrow },
+        { "ShootRight", KeyCode.RightArrow },
+        { "Melee", KeyCode.E },
+        { "Heal", KeyCode.R },
+        { "Interact", KeyCode.F },
+        { "SwitchWeapon", KeyCode.Tab },
+        { "Pause", KeyCode.Escape },
+        { "Inventory", KeyCode.I },
+
+    };
+}
