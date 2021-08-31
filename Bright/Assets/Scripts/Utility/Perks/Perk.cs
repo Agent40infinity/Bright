f5105ce3defd2303394cb1dafdd9da0760dd209d
Aiden Nathan <agent9@y7mail.com>
2021-08-31 18:41:08 +1000

    Perk Base System Implemented

---
+++ Bright/Assets/Scripts/Utility/Perks/Perk.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Perk : MonoBehaviour
+{
+    private string perkName;
+    private Sprite portrait;
+    private PerkType perkType;
+
+    public string Name
+    {
+        get { return perkName; }
+        set { perkName = value; }
+    }
+
+    public Sprite Portrait
+    {
+        get { return portrait; }
+        set { portrait = value; }
+    }
+
+    public PerkType PerkType
+    {
+        get { return perkType; }
+        set { perkType = value; }
+    }
+
+    public void ItemCreation()
+    { 
+        
+    }
+}
---
+++ Bright/Assets/Scripts/Utility/Perks/PerkData.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerkData : MonoBehaviour
+{
+    public static Perk AddPerk(string perkName)
+    {
+        string name = "";
+        PerkType perkType = PerkType.None;
+
+        string path = "Sprites/Perks/" + perkType.ToString();
+
+        Perk temp = new Perk()
+        {
+            Name = name,
+            PerkType = perkType,
+            Portrait = Resources.Load<Sprite>(path),
+        };
+
+        return temp;
+    }
+}
+
+public enum PerkType
+{ 
+    None
+}
