e9c074587db4cc0731f64b5f5d36d5ae615ecd31
Keynxd <jacobnicholls71@gmail.com>
2021-09-09 03:06:09 +1000

    MushyMushyJR
    MushroomJR v1, rockin into the crib

---
+++ Bright/Assets/Scripts/Enemies/EnemyBullet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    public float movementSpeed;
+
+    private Transform target;
+    private Vector2 targetLoc;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        targetLoc = new Vector2(target.position.x, target.position.y);
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, targetLoc, movementSpeed * Time.deltaTime);
+
+        if(transform.position.x == targetLoc.x && transform.position.y == targetLoc.y)
+        {
+            Explode();
+        }
+    }
+
+    void Explode()
+    {
+        // implement particle pew pew
+    }
+}
---
+++ Bright/Assets/Scripts/Enemies/TempMushroom.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempMushroom : MonoBehaviour
+{
+    public float delayBetweenJumps;
+    public float jumpSpeed;
+    public float jumpDistance;
+    public float targetDistance;
+    public float delayBetweenAttacks;
+
+    private float jumpTimer;
+    private float attackTimer;
+
+    public int health;
+    public GameObject bullet;
+
+    private Rigidbody2D rb;
+    private Transform target;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        jumpTimer -= Time.deltaTime;
+        attackTimer -= Time.deltaTime;
+        rb.velocity = rb.velocity * 0.99f;
+
+        if (health <= 0)
+        {
+            Death();
+        }
+
+        if (Vector2.Distance(transform.position, target.position) > targetDistance && jumpTimer <= 0)
+        {
+            jumpTimer = delayBetweenJumps;
+            Jump();
+        }
+        else if (Vector2.Distance(transform.position, target.position) < targetDistance && attackTimer <=0)
+        {
+            attackTimer = delayBetweenAttacks;
+            Attack();
+        }
+    }
+
+    void Jump()
+    {
+        var direction = Vector3.zero;
+        direction = target.position - transform.position;
+        rb.AddRelativeForce(direction.normalized * jumpSpeed * jumpDistance, ForceMode2D.Force);
+    }
+
+    void Attack()
+    {
+        Instantiate(bullet, transform.position, Quaternion.identity);
+    }
+
+    void Death()
+    {
+        Destroy(gameObject);
+    }
+}
