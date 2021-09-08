f0889ff7ece46087f1223ce50c8654c3d4f8e094
Aiden Nathan <agent9@y7mail.com>
2021-09-09 00:44:04 +1000

    Update Room.cs

--- Bright/Assets/Scripts/Generation/Room.cs
+++ Bright/Assets/Scripts/Generation/Room.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Room
 {
+    [Header("Attributes")]
     public string type;
     public int difficulty;
     public int variant;
+
+    [Header("Active Slot")]
     public GameObject room;
+    public VisitedState visitedState = VisitedState.Empty;
 
     public Room(string typeInput, int difficultyInput, int variantInput)
     {
@@ -20,4 +24,11 @@ public class Room
     {
         type = typeInput;
     }
+}
+
+public enum VisitedState
+{ 
+    Empty,
+    Occupied,
+    Clear
 }
