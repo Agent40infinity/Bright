a2c8a23eac5ce02f460701e3b8bbc1599619fb17
Keynxd <jacobnicholls71@gmail.com>
2021-09-09 12:47:52 +1000

    playertakesdmg
    player now takes damage from the first enemy

---
+++ Bright/Assets/Scripts/Enemies/EnemyHealth.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [Header("Health")]
+    public int maxHealth = 6;
+    public int curHealth; 
+
+    public void Start()
+    {
+        curHealth = maxHealth;
+    }
+
+    public void Update()
+    {
+        HealthCheck();
+    }
+
+    public void HealthCheck()
+    {
+        if (curHealth <= 0)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        Destroy(gameObject);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        curHealth--;
+    }
+}
