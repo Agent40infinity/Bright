9c9b8e2d54cb919b2d198f623870e26b3dc20c91
Aiden Nathan <agent9@y7mail.com>
2021-10-05 02:51:38 +1100

    Implemented Base GameEvents

--- Bright/Assets/Scripts/Utility/Events/GameEvents.cs
+++ Bright/Assets/Scripts/Utility/Events/GameEvents.cs
@@ -6,10 +6,32 @@ public class GameEvents : MonoBehaviour
 {
     [Header("General")]
     public List<GameObject> Objectives;
+    public bool[] Checks;
 
     public EventState eventState;
+    public TutorialState tutorialState;
+
+    public void Start()
+    {
+        SelectEvent();
+    }
+
+    public void SelectEvent()
+    {
+        switch (eventState)
+        {
+            case EventState.Tutorial:
+                Checks = new bool[System.Enum.GetValues(typeof(TutorialState)).Length];
+                break;
+        }
+    }
 
     public void Update()
+    {
+        Event();
+    }
+
+    public void Event()
     {   
         switch (eventState)
         {
@@ -21,12 +43,93 @@ public class GameEvents : MonoBehaviour
 
     public void TutorialEvent()
     {
+        KeyCode[] keybinds = null;
+        GameObject objective;
+        int index = (int)tutorialState;
 
+        switch (tutorialState)
+        {
+            case TutorialState.Move:
+                keybinds = new KeyCode[] { GameManager.keybind["MoveUp"], GameManager.keybind["MoveDown"], GameManager.keybind["MoveLeft"], GameManager.keybind["MoveRight"] };
+                break;
+            case TutorialState.Shoot:
+                keybinds = new KeyCode[] { GameManager.keybind["ShootUp"], GameManager.keybind["ShootDown"], GameManager.keybind["ShootLeft"], GameManager.keybind["ShootRight"] };
+                break;
+            case TutorialState.Switch:
+                keybinds = new KeyCode[] { GameManager.keybind["SwitchWeapon"] };
+                break;
+            case TutorialState.Melee:
+                keybinds = new KeyCode[] { GameManager.keybind["Melee"] };
+                break;
+            case TutorialState.SwitchWorlds:
+                keybinds = new KeyCode[] { GameManager.keybind["TrueSight"] };
+                break;
+            case TutorialState.Interact:
+                keybinds = new KeyCode[] { GameManager.keybind["Interact"] };
+                break;
+        }
+
+        SwitchStateCheck(index, keybinds);
+    }
+
+    public void SwitchStateCheck(int index, KeyCode[] keybinds)
+    {
+        if (!Checks[index])
+        {
+            Checks[index] = KeybindCheck(keybinds);
+        }
+        else
+        {
+            NextState(index);
+        }
+    }
+
+    public bool KeybindCheck(KeyCode[] keycode)
+    {
+        if (keycode == null)
+        {
+            return true;
+        }
+
+        for (int i = 0; i < keycode.Length; i++)
+        {
+            if (Input.GetKeyDown(keycode[i]))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void NextState(int index)
+    {
+        int newIndex = index + 1;
+
+        if (newIndex != System.Enum.GetValues(typeof(TutorialState)).Length)
+        {
+            tutorialState = (TutorialState)System.Enum.GetValues(typeof(TutorialState)).GetValue(newIndex);
+        }
+        else
+        {
+            eventState = EventState.None;
+            Debug.Log("My tutorial worked when I feel brain damaged fuckers, suck a dick I'm a God");
+        }
     }
 }
 
 public enum EventState
-{ 
+{
     Tutorial,
     None
+}
+
+public enum TutorialState
+{ 
+    Move,
+    Shoot,
+    Switch,
+    Melee,
+    SwitchWorlds,
+    Interact,
 }
